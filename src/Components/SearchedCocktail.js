79cd6de8638fd5d25fbc2ca7a1ce9e1f91a1e03f
ty <tylerpmatthews1998@gmail.com>
2021-01-28 22:10:28 -0500

    cleaned up for Vercel

--- src/Components/SearchedCocktail.js
+++ src/Components/SearchedCocktail.js
@@ -4,7 +4,7 @@ const SearchedCocktail = (props) => {
   return (
     <div key={props.item.idDrink}>
       <h2>{props.item.strDrink}</h2>
-      <img src={props.item.strDrinkThumb} />
+      <img src={props.item.strDrinkThumb} alt={props.item.strDrink}/>
     </div>
   );
 };
